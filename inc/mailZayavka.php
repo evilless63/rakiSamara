1948935f18c8403fd4d61595565c01505919b643
Vitaliy Emelianov <Vitaliy030589@gmail.com>
2016-09-10 18:27:22 +0400

    fix script email

--- inc/mailZayavka.php
+++ inc/mailZayavka.php
@@ -13,7 +13,7 @@
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        $headers .= "From: Клиент с сайта Продажа Раков <from@example.com>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 
 ?>
--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
-$('.bestReceptsMain').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+$(document).ready(function(){
 
-
-$('.bestReceptsLink').hover(
-  function() {
-    $(this).children('.bestReceptsImg').addClass('imageHover');
-    $(this).children('.bestReceptsDesc').css('display', 'block');
-  }, function() {
-    $(this).children('.bestReceptsImg').removeClass('imageHover');
-    $(this).children('.bestReceptsDesc').css('display', 'none');
-  }
-);
-
-
-$("#zayavka").click(function(){
-
-    var name = $("#itemScheduleFormName").val();
-    var phone = $("#itemScheduleFormPhone").val();
-
-      $.ajax({
-          type: "POST",
-          url: "../inc/mailZayavka.php",
-          data: { name: name, phone: phone }
-        }).done(function() {
-          alert( "Заявка успешно отправлена");
-          $("#itemScheduleFormName").val("")
-          var phone = $("#itemScheduleFormPhone").val("");
-        });
+  $('.bestReceptsMain').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
   });
 
 
-$('#tab-container').easytabs();
+  $('.bestReceptsLink').hover(
+    function() {
+      $(this).children('.bestReceptsImg').addClass('imageHover');
+      $(this).children('.bestReceptsDesc').css('display', 'block');
+    }, function() {
+      $(this).children('.bestReceptsImg').removeClass('imageHover');
+      $(this).children('.bestReceptsDesc').css('display', 'none');
+    }
+  );
+
+
+  $(".submitZayavka").click(function(){
+
+      var name = $("#itemScheduleFormName").val();
+      var phone = $("#itemScheduleFormPhone").val();
+
+        $.ajax({
+            type: "POST",
+            url: "../inc/mailZayavka.php",
+            data: { name: name, phone: phone }
+          }).done(function() {
+            alert( "Заявка успешно отправлена");
+            $("#itemScheduleFormName").val("")
+            var phone = $("#itemScheduleFormPhone").val("");
+          });
+    });
+
+
+  $('#tab-container').easytabs();
+
+});
+
